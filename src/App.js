93bee6f547857cd4eae363398b657d684fa22a24
Dima Matusik <k.syndikat@gmail.com>
2020-03-09 02:32:20 +0300

    add new components

--- src/App.js
+++ src/App.js
@@ -1,45 +1,21 @@
 import React from 'react';
 import './App.css';
+import TodoListHeader from "./components/TodoListHeader/TodoListHeader";
+import TodoListTasks from "./components/TodoListTasks/TodoListTasks";
+import TodoListFooter from "./components/TodoListFooter/TodoListFooter";
 
 class App extends React.Component {
-    render = () => {
-        return (
-            <div className="App">
-                <div className="todoList">
-                    <div className="todoList-header">
-                        <h3 className="todoList-header__title">What to Learn</h3>
-                        <div className="todoList-newTaskForm">
-                            <input type="text" placeholder="New task name"/>
-                            <button>Add</button>
-                        </div>
-                    </div>
-                    <div className="todoList-tasks">
-                        <div className="todoList-task">
-                            <input type="checkbox" checked={true}/>
-                            <span>CSS</span>
-                        </div>
-                        <div className="todoList-task">
-                            <input type="checkbox" checked={false}/>
-                            <span>JS</span>
-                        </div>
-                        <div className="todoList-task">
-                            <input type="checkbox" checked={false}/>
-                            <span>ReactJS</span>
-                        </div>
-                        <div className="todoList-task">
-                            <input type="checkbox" checked={true}/>
-                            <span>Patterns</span>
-                        </div>
-                    </div>
-                    <div className="todoList-footer">
-                        <button>All</button>
-                        <button>Completed</button>
-                        <button>Active</button>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render = () => {
+    return (
+      <div className="App">
+        <div className="todoList">
+          <TodoListHeader/>
+          <TodoListTasks/>
+          <TodoListFooter/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
