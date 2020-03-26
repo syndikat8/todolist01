da9175c25adfb0117c8839efce101800f953bcf1
Dima Matusik <k.syndikat@gmail.com>
2020-03-27 02:26:42 +0300

    add new state

--- src/components/TodoListTasks/TodoListTask/TodoListTask.js
+++ src/components/TodoListTasks/TodoListTask/TodoListTask.js
@@ -4,18 +4,15 @@ import React from 'react';
 class TodoListTask extends React.Component {
 
   onIsDoneChanged = (e) => {
-    this.props.changeStatus(this.props.task, e.currentTarget.checked)
+    this.props.changeStatus(this.props.task, e.currentTarget.checked);
+
 
   }
 
   render = () => {
     return (
         <div className="todoList-task">
-          <input
-            type="checkbox"
-            checked={this.props.isDone}
-            onCahnge={this.onIsDoneChanged}
-          />
+          <input type="checkbox" checked={this.props.task.isDone} onChange={this.onIsDoneChanged}/>
           <span>{this.props.task.title} priority: {this.props.task.priority}</span>
         </div>
     );
--- src/components/TodoListTasks/TodoListTasks.js
+++ src/components/TodoListTasks/TodoListTasks.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import TodoListTask from "./TodoListTask/TodoListTask";
 import PropTypes from 'prop-types';
-import TodoListFooter from "../TodoListFooter/TodoListFooter";
+
 
 class TodoListTasks extends React.Component {
   render = () => {
-    let taskElements = this.props.tasks.map(task =>{
+    let taskElements = this.props.tasks.map(task => {
       return <TodoListTask
         changeStatus={this.props.changeStatus}
         task={task}
-        // title={task.title}
-        // isDone={task.isDone}
-        // priority={task.priority}
       />
     });
     return (
