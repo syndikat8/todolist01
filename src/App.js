2857549895cdb954491b09cee68cf963610aaec5
Dima Matusik <k.syndikat@gmail.com>
2020-04-07 14:23:39 +0300

    refactor if else to TodolistTasks

--- src/App.js
+++ src/App.js
@@ -26,18 +26,18 @@ class App extends React.Component {
       isDone: true,
       priority: "low"
     };
-    let newTasks = [...this.state.tasks, newTask]
+    let newTasks = [...this.state.tasks, newTask];
     this.setState({
       tasks: newTasks
     })
-  }
+  };
 
 
   changeFilter = (newFilterValue) => {
     this.setState({
       filterValue: newFilterValue
     })
-  }
+  };
 
   changeStatus = (task, isDone) => {
     let newTasks = this.state.tasks.map(t => {
@@ -46,11 +46,11 @@ class App extends React.Component {
       } else {
         return {...t, isDone: isDone}
       }
-    })
+    });
     this.setState({
       tasks: newTasks
     })
-  }
+  };
 
   render = () => {
 
@@ -58,20 +58,14 @@ class App extends React.Component {
       <div className="App">
         <div className="todoList">
           <TodoListHeader addTask={this.addTask}/>
-
           <TodoListTasks
             changeStatus={this.changeStatus}
             tasks={this.state.tasks.filter(t => {
-            if (this.state.filterValue === "All") {
-              return true;
-            }
-            if (this.state.filterValue === "Active") {
-              return t.isDone === false;
-            }
-            if (this.state.filterValue === "Completed") {
-              return t.isDone === true;
-            }
-          })}/>
+              switch (this.state.filterValue) {
+                case "All": return true;
+                case "Completed": return t.isDone;
+                case "Active": return !t.isDone;
+              }})}/>
           <TodoListFooter filterValue={this.state.filterValue}
                           changeFilter={this.changeFilter}
           />
