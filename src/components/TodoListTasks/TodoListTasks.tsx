42d17be3a2425e7818cc902643734bc5bc28b125
Dima Matusik <k.syndikat@gmail.com>
2020-06-05 19:52:31 +0300

    add ts for todoTasks

---
+++ src/components/TodoListTasks/TodoListTasks.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import TodoListTask from "./TodoListTask/TodoListTask";
+import { TaskType } from '../../types/entities';
+
+type OnPropsType = {
+    tasks: Array<TaskType>
+    deliteTask: () => void
+    changeTitle: () => void
+    changeStatus: () => void
+}
+
+
+class TodoListTasks extends React.Component<OnPropsType> {
+
+    render = () => {
+        let taskElements = this.props.tasks.map(task => {
+            return <TodoListTask
+                key={task.id}
+                deliteTask={this.props.deliteTask}
+                changeTitle={this.props.changeTitle}
+                changeStatus={this.props.changeStatus}
+                task={task}
+            />
+        });
+        return (
+            <div className="todoList-tasks">
+                {taskElements}
+            </div>
+        );
+    }
+}
+
+export default TodoListTasks;
